a981e91f032f481ed2da51266c437b55e5da77f2
Yevhenii Harbuzniak <fnostric@gmail.com>
2015-02-16 13:05:22 +0200

    initial

---
+++ frontend/config/main-local.php
@@ -0,0 +1,21 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'C_ePxGXvy4LGPSHOod3kJi1qp6AOe1_m',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
